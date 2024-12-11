2a08dd57e5910d661e4afbfa8dd545f835329e08
zixian <2218319940@qq.com>
2024-12-11 17:09:31 +0000

    continue_development

---
+++ run/run_gat.sh
@@ -0,0 +1,50 @@
+# Define the paths as variables
+PYTHON_EXEC=/home/zixian/.conda/envs/semantic_graph_code_clone/bin/python
+PROJECT_DIR=/home/zixian/PycharmProjects/semantic_graph_code_code_clone
+SCRIPT=$PROJECT_DIR/main.py
+CONFIG_DIR=$PROJECT_DIR/configs/benchmark_without_value_GAT/BCB
+
+
+$PYTHON_EXEC $SCRIPT --config $CONFIG_DIR/AST_CFG_DFG_FA_GAT_BCB.json &
+
+
+# Wait for 30 minutes (1800 seconds)
+echo "Waiting for 5 minutes before executing the next command..."
+sleep 1200
+$PYTHON_EXEC $SCRIPT --config $CONFIG_DIR/AST_CFG_DFG_GAT_BCB.json &
+
+
+# Wait for 30 minutes (1800 seconds)
+echo "Waiting for 5 minutes before executing the next command..."
+sleep 1200
+$PYTHON_EXEC $SCRIPT --config $CONFIG_DIR/AST_CFG_FA_GAT_BCB.json &
+
+# Wait for 30 minutes (1800 seconds)
+echo "Waiting for 5 minutes before executing the next command..."
+sleep 1200
+$PYTHON_EXEC $SCRIPT --config $CONFIG_DIR/AST_CFG_GAT_BCB.json &
+
+
+# Wait for 30 minutes (1800 seconds)
+echo "Waiting for 5 minutes before executing the next command..."
+sleep 1200
+$PYTHON_EXEC $SCRIPT --config $CONFIG_DIR/AST_DFG_FA_GAT_BCB.json &
+
+# Wait for 30 minutes (1800 seconds)
+echo "Waiting for 5 minutes before executing the next command..."
+sleep 1200
+$PYTHON_EXEC $SCRIPT --config $CONFIG_DIR/AST_DFG_GAT_BCB.json &
+
+# Wait for 30 minutes (1800 seconds)
+echo "Waiting for 5 minutes before executing the next command..."
+sleep 1200
+$PYTHON_EXEC $SCRIPT --config $CONFIG_DIR/AST_FA_GAT_BCB.json &
+
+# Wait for 30 minutes (1800 seconds)
+echo "Waiting for 5 minutes before executing the next command..."
+sleep 1200
+$PYTHON_EXEC $SCRIPT --config $CONFIG_DIR/AST_GAT_BCB.json &
+
+
+wait
+echo "Both processes have completed."
